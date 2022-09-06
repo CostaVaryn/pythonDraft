9b4937b8c1694d3be35c1b3547046f505849fa53
Costa Varyn <d3tug@yandex.ru>
2022-09-06 13:36:11 +0300

    update test.py

--- venv/Scripts/test.py
+++ venv/Scripts/test.py
@@ -1,3 +1,5 @@
+'''from random import randint'''
+
 def slow(n, tubes):
     ans = 0
     for i in range(n):
@@ -8,9 +10,39 @@ def slow(n, tubes):
                     ans += 1
     return ans
 
+def fast(n, tubes):
+    ans = 0
+    for i in range(n):
+        l = i + 2
+        r = i + 2
+        for j in range(i + 1, n):
+            while l < n and tubes[i]**2 + tubes[j]**2 >= tubes[l]**2:
+                l += 1
+            while r < n and tubes[i] + tubes[j] > tubes[r]:
+                r += 1
+            ans += r - l
+    return ans
+
+'''
+while True:
+    n = 4
+    tubes = []
+    for i in range(n):
+        tubes.append(randint(1, 10))
+        tubes.sort()
+    print(*tubes)
+    ansSlow = slow(n, tubes)
+    ansFast = fast(n, tubes)
+    print(ansSlow, ansFast)
+    if ansSlow != ansFast:
+        break
+'''
+
 n = int(input())
 tubes = list(map(int, input().split()))
-print(slow(n, tubes))
+print(fast(n, tubes))
+
+"print(slow(n, tubes)"
 
 '''
 Input:
