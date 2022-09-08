cfa2d553d5972ca0794c9068afb408dc57056e25
Costa Varyn <d3tug@yandex.ru>
2022-09-08 11:42:27 +0300

    add BreakPalindrome.py

---
+++ venv/Scripts/BreakPalindrome.py
@@ -0,0 +1,31 @@
+'''
+Input:
+abba
+Output:
+aaba
+
+or
+
+Input:
+a
+Output:
+
+'''
+
+def sol(s):
+    if len(s) == 1:
+        return ''
+    middle = len(s) // 2
+    flag = False
+    for i in range(middle):
+        if s[i] != 'a':
+            ans = s[:i] + 'a' + s[i+1:]
+            flag = True
+            break
+    if flag:
+        return ans
+    else:
+        return s[:-1] + 'b'
+
+s = input()
+print(sol(s))
